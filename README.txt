324d6a54f9b63cf831ffab1b360f6e262e6f2a07
Avant Baker <b.avant89@gmail.com>
2016-04-04 12:01:04 -0400

    finished the initial routing to allow Jason to go in and do his stuff. Changed Pages to Steps and corrected the layout for the inner content container

---
+++ README.txt
@@ -0,0 +1,26 @@
+Application Structure
+
+The app consists of 5 main pages with an approve proof page and download page
+
+The first 5 form pages are called steps and have pagination. They Include:
+
+1. Your Information
+2. Select a Template
+3. Basic tournament Information
+4. Tournament Description
+5. Tournament Contact Information
+
+Biggest Factor: Routing
+
+Main Content Box is split up into Left and Right sides. The left contains the Forms that
+will be cycled through. The right a presentation of the Poster. When a user enters their 
+tournament information it should update on the right side. The presentation of the poster
+is viewable on all pages except the last.
+
+The Header, Middle and Footer will be the only sections in the index.html
+
+Your Information is the Base Url
+Select a template will be /select-
+
+
+
--- app.js
+++ app.js
@@ -1,28 +1,64 @@
-var sportNgin = angular.module("sportNgin", ["ngResource", "ngRoute"]);
+var sportNgin = angular.module("sportNgin", ["ngResource", "ui.router"]);
 
-sportNgin.config(function($routeProvider){
-
-	$routeProvider
-
-	.when('/', {
-		templateUrl: 'pages/home.html',
-		controller: 'homeCntrl'
-	})
-
-});
+sportNgin.config([
+	'$stateProvider', '$urlRouterProvider',
+	function ( $stateProvider, $urlRouterProvider ) {
+		
+		$stateProvider
+		
+			.state('home', {
+				url: '/home',
+				controller: 'homeCntrl',
+				templateUrl: 'steps/home.html'
+			})
+			
+			.state('home.yourInfo', {
+				url: '/info',
+				controller: 'homeCntrl',
+				templateUrl: 'steps/home-yourInfo.html'
+			})
+			
+			.state('home.selectTemplate', {
+				url: '/selectTemplate',
+				controller: 'homeCntrl',
+				templateUrl: 'steps/home-selectTemplate.html'
+			})
+			
+			.state('home.tournamentInfo', {
+				url: '/tournamentInfo',
+				controller: 'homeCntrl',
+				templateUrl: 'steps/home-tournamentInfo.html'
+			})
+			
+			.state('home.tournamentDes', {
+				url: '/tournamentDescription',
+				controller: 'homeCntrl',
+				templateUrl: 'steps/home-tournamentDes.html'
+			})
+			
+			.state('home.tournamentContact', {
+				url: '/tournamentContact',
+				controller: 'homeCntrl',
+				templateUrl: 'steps/home-tournamentContact.html'
+			});
+			
+		
+		$urlRouterProvider.otherwise('/home/info');
+	}	
+]);
 
 sportNgin.service( '', function(){
 
 
 });
 
-sportNgin.controller('homeCntrl', [ "$scope", "$routeParams", "$log", function($scope, $routeParams, $log){
+sportNgin.controller('homeCntrl', [ "$scope", "$log", function($scope, $log){
 	
 
 	
 }]);
 
-sportNgin.controller('', [ "$scope", "$log", "$resource", "$routeParams", function($scope, $log, $resource, $routeParams){
+sportNgin.controller('', [ "$scope", "$log", "$resource", function($scope, $log, $resource){
 	
 
 }]);
