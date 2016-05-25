4f15244bb5515d28d5370f4f598059a3ca8d70c7
lehmrun <lehmrun@gmail.com>
2016-05-25 15:21:39 -0400

    Poster template rework

---
+++ mpdf/ttfontdata/robotocondensed.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='RobotoCondensed-Regular';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 711,
+  'XHeight' => 528,
+  'FontBBox' => '[-737 -271 998 1056]',
+  'Flags' => 4,
+  'Ascent' => 950,
+  'Descent' => -250,
+  'Leading' => 0,
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 392,
+);
+$unitsPerEm=2048;
+$up=-73;
+$ut=49;
+$strp=250;
+$strs=50;
+$ttffile='/Applications/MAMP/htdocs/sportNgin/mpdf/ttfonts/RobotoCondensed-Regular.ttf';
+$TTCfontID='0';
+$originalsize=161524;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='robotocondensed';
+$panose=' 0 0 2 0 0 0 0 0 0 0 0 0';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 750, -250, 50
+// usWinAscent/usWinDescent = 950, -250
+// hhea Ascent/Descent/LineGap = 928, -244, 0
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
